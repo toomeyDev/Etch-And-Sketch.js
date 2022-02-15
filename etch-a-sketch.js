cae2e515fdc747c337574cdc121577b775d6d4b3
Matthew Toomey <mtoomeydev@gmail.com>
2022-02-15 15:48:14 -0500

    Implement basic cell-coloring, 16x16 grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,7 +3,9 @@ for(let i = 0; i < 16; i++){
     for(let r = 0; r < 16; r++){
         let square = document.createElement('div');
         square.setAttribute('class', 'gridSquare');
-        square.textContent="square";
+        square.addEventListener("mouseover", function (event){
+            event.target.style.backgroundColor = 'green';
+        });
         squaresGrid.appendChild(square);
     }
 }
