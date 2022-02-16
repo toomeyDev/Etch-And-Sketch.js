5e64142333514b4e4fbf3b8a05dc3a5b578b9c41
Matthew Toomey <mtoomeydev@gmail.com>
2022-02-15 20:49:06 -0500

    start implementing different size grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,19 @@
 const squaresGrid = document.querySelector('.grid');
-for(let i = 0; i < 16; i++){
-    for(let r = 0; r < 16; r++){
-        let square = document.createElement('div');
-        square.setAttribute('class', 'gridSquare');
-        square.addEventListener("mouseover", function (event){
-            event.target.style.backgroundColor = 'green';
-        });
-        squaresGrid.appendChild(square);
+drawGrid(16);
+
+function drawGrid(size){
+    squaresGrid.innerHTML = "";
+    for(let i = 0; i < size; i++){
+        for(let r = 0; r < size; r++){
+            let square = document.createElement('div');
+            square.setAttribute('class', 'gridSquare');
+            square.addEventListener("mouseover", function (event){
+                event.target.style.backgroundColor = 'black';
+            });
+            squaresGrid.appendChild(square);
+        }
     }
 }
-
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', function() {
     let squares = squaresGrid.querySelectorAll('div');
@@ -18,4 +22,14 @@ resetButton.addEventListener('click', function() {
             currentValue.style.backgroundColor = 'white';
         } 
     )
+    do{
+        let response = prompt("Enter pixel size (max 100): ");
+        if(response <= 0 || response > 100 || isNaN(response)){
+            alert("Please enter a valid non-zero number between 1-100.");
+            continue;
+        } else {
+            drawGrid(response);
+            break;
+        }      
+    }while(true)
 });
